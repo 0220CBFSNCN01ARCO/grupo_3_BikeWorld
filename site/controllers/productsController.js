0ad84f1c9256e5b1e42902ae5a36a00411b34f02
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-07-25 12:22:28 -0300

    ProductsController: Migrado a sequelize
    Se actualizaron todos los métodos para que ahora usen Sequelize y sean
    asíncronos

--- site/controllers/productsController.js
+++ site/controllers/productsController.js
@@ -1,81 +1,135 @@
-const fs = require('fs')
+const db = require('../database/models')
 const path = require('path')
 
-var products = JSON.parse(fs.readFileSync('site/src/data/products.json', 'utf-8'))
-
-const updateProducts = callback => {
-  fs.writeFile('site/src/data/products.json', JSON.stringify(products, null, 2), err => {
-    if (err) {
-      throw err
-    }
-
-    callback()
-  })
-}
-
 const getProductImagePath = imageFilename => {
-  return path.resolve('/images/products', imageFilename)
+  return path.join('/images/products', imageFilename)
 }
+
+const toThousand = number => number.toString()
+  .require('.', ',')
+  .require(/\B(?=(\d{3})+(?!\d))/g, '.')
 
 module.exports = {
-  showProductList: (req, res) => res.render('productList', {
-    products: products,
-    getProductImagePath: getProductImagePath
-  }),
-  showProductCreationForm: (req, res) => res.render('productCreationForm'),
-  createProduct: (req, res) => {
-    products.push({
-      id: Number(products[products.length - 1].id + 1),
-      name: req.body.name,
-      price: Number(req.body.price),
-      discount: Number(req.body.discount),
-      category: req.body.category,
-      description: req.body.description,
-      image: req.file.filename,
-      status: req.body.status
-    })
-
-    updateProducts(() => res.redirect('/products'))
-  },
-  showProductDetails: (req, res) => res.render('productDetails', {
-    product: products.find(product => {
-      return product.id === Number(req.params.id)
-    }),
-    getProductImagePath: getProductImagePath
-  }),
-  showProductEditForm: (req, res) => res.render('productEditForm', {
-    product: products.find(product => {
-      return product.id === Number(req.params.id)
-    }),
-    getProductImagePath: getProductImagePath
-  }),
-  editProduct: (req, res) => {
-    const productIndex = products.findIndex(product => {
-      return product.id === Number(req.params.id)
-    })
-
-    products[productIndex] = {
-      id: Number(req.params.id),
-      name: req.body.name,
-      price: Number(req.body.price),
-      discount: Number(req.body.discount),
-      category: req.body.category,
-      description: req.body.description,
-      status: req.body.status
+  showProductList: async (req, res) => {
+    try {
+      res.render('productList', {
+        products: await db.Product.findAll(),
+        getProductImagePath: getProductImagePath,
+        toThousand: toThousand,
+        categories: await b.ProductCategory.findAll(),
+        states: await db.ProductStatus.findAll()
+      })
+    } catch (err) {
+      console.error(err)
     }
-
-    // No `image: req.file?.filename`? Why JS :(
-    if (req.file) {
-      products[productIndex].image = req.file.filename
-    }
-
-    updateProducts(() => res.redirect('/products'))
   },
-  deleteProduct: (req, res) => {
-    products = products.filter(product => {
-      return product.id !== Number(req.params.id)
-    })
+  showProductCreationForm: async (req, res) => {
+    try {
+      res.render('productCreationForm', {
+        categories: await db.ProductCategory.findAll(),
+        states: await db.ProductStatus.findAll()
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  createProduct: async (req, res) => {
+    try {
+      let category = await db.ProductCategory.findOne({ where: { name: req.body.category } })
+      if (category === null) {
+        category = await db.ProductCategory.create({ name: req.body.category })
+      }
 
-    updateProducts(() => res.redirect('/products'))
+      let status = await db.ProductStatus.findOne({ where: { name: req.body.status } })
+      if (status === null) {
+        status = await db.ProductStatus.create({ name: req.body.status })
+      }
+
+      await db.Product.create({
+        name: req.body.name,
+        price: req.body.price,
+        discount: req.body.discount,
+        productCategoryId: category.id,
+        description: req.body.description,
+        image: req.file.filename,
+        productStatusId: status.id
+      })
+
+      res.redirect('/products')
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  showProductDetails: async (req, res) => {
+    try {
+      res.render('productDetails', {
+        product: await db.Product.findByPk(req.params.id),
+        getProductImagePath: getProductImagePath,
+        newlineToBr: text => text.replace(/\r\n/g, '<br>')
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  showProductEditForm: async (req, res) => {
+    try {
+      res.render('productEditForm', {
+        product: await db.Product.findByPk(req.params.id, {
+          include: [
+            { association: 'status' },
+            { association: 'category' }
+          ]
+        }),
+        getProductImagePath: getProductImagePath,
+        categories: await db.ProductCategory.findAll(),
+        states: await db.ProductStatus.findAll()
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  editProduct: async (req, res) => {
+    try {
+      let category = await db.ProductCategory.findOne({ where: { name: req.body.category } })
+      if (category === null) {
+        category = await db.ProductCategory.create({ name: req.body.category })
+      }
+
+      let status = await db.ProductStatus.findOne({ where: { name: req.body.status } })
+      if (status === null) {
+        status = await db.ProductStatus.create({ name: req.body.status })
+      }
+
+      await db.Product.update({
+        name: req.body.name,
+        price: req.body.price,
+        discount: req.body.discount,
+        productCategoryId: category.id,
+        description: req.body.description,
+        productStatusId: status.id
+      }, { where: { id: req.params.id } })
+
+      // No `image: req.file?.filename`? Why JS :(
+      if (req.file) {
+        await db.Product.update({ image: req.file.filename }, { where: { id: req.params.id } })
+      }
+
+      res.redirect('/products')
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  deleteProduct: async (req, res) => {
+    try {
+      await db.Product.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+
+      res.redirect('/products')
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
