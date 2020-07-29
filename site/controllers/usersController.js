98fd99dcf3c4db4945abc3f375448c616e5a79ae
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-07-29 19:51:20 -0300

    UsersController: Implementación inicial
    Se terminó la implementación inicial del controlador de usuarios

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -1,3 +1,63 @@
+const createHttpError = require('http-errors')
+const db = require('../database/models')
+
 module.exports = {
-  showRegistrationForm: (req, res) => res.render('registrationForm')
+  showRegistrationForm: (req, res) => {
+    res.render('registrationFrom')
+  },
+  createUser: async (req, res) => {
+    try {
+      const userCategory = await db.UserCategory.findByPk(1)
+
+      await db.User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        userCategoryId: userCategory.id,
+        image: req.file.filename
+      })
+
+      res.redirect('/')
+    } catch (err) {
+      createHttpError(500)
+      console.error(err)
+    }
+  },
+  editUser: async (req, res) => {
+    try {
+      const userCategory = await db.UserCategory.findByPk(1)
+
+      await db.User.update({
+        firstName: req.body.fistName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        userCategoryId: userCategory.id
+      }, { where: req.params.id })
+
+      if (req.file) {
+        await db.User.update({ image: req.file.filename }, { where: req.params.id })
+      }
+
+      res.redirect('/')
+    } catch (err) {
+      createHttpError(500)
+      console.error(err)
+    }
+  },
+  showUserDetails: async (req, res) => {
+    try {
+      res.render('userDetails', {
+        user: await db.User.findByPk(req.params.id, {
+          include: [
+            { association: 'category' }
+          ]
+        })
+      })
+    } catch (err) {
+      createHttpError(500)
+      console.error(err)
+    }
+  }
 }
