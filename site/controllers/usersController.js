cf1bcf805aa8cdf5191f83043f2f85aa04e9d041
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-08-02 19:29:59 -0300

    UsersControllers: Mejora general de las funciones del controlador
    Se corrigieron los nombres de las funciones, se mejoró la lógica para cada uno,
    se agregó la función de loguear usuarios y la de borrar usuarios. El logueo se
    realiza mediante JSON Web Token y Cookies

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -1,44 +1,31 @@
 import db from '../database/models'
 import createHttpError from 'http-errors'
+import { hash, compare } from 'bcrypt'
+import { sign as _sign, verify as _verify } from 'jsonwebtoken'
+import { promisify } from 'util'
+
+const sign = promisify(_sign)
+const verify = promisify(_verify)
 
 export const showRegistrationForm = (req, res) => res.render('registrationForm')
 
-export const createUser = async (req, res) => {
+export const registerUser = async (req, res) => {
   try {
-    const userCategory = await db.UserCategory.findByPk(1)
-
-    await db.User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
+    const user = await db.User.create({
+      firstName: req.body.firstName || '',
+      lastName: req.body.lastName || '',
       email: req.body.email,
-      password: req.body.password,
-      userCategoryId: userCategory.id,
-      image: req.file.filename
+      password: await hash(req.body.password, 10),
+      userAdmin: false
     })
-
-    res.redirect('/')
-  } catch (err) {
-    createHttpError(500)
-    console.error(err)
-  }
-}
-
-export const editUser = async (req, res) => {
-  try {
-    const userCategory = db.UserCategory.findByPk(1)
-
-    await db.User.update({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      userCategoryId: userCategory.id
-    }, { where: req.params.id })
 
     if (req.file) {
-      await db.User.update({ image: req.file.filename }, { where: req.params.id })
+      user.image = req.file.filename
+      await user.save()
     }
 
+    const token = await sign({ user: { email: user.email } }, 'our secret')
+    res.cookie('token', token)
     res.redirect('/')
   } catch (err) {
     createHttpError(500)
@@ -46,15 +33,90 @@ export const editUser = async (req, res) => {
   }
 }
 
-export const showUserDetails = async (req, res) => {
+export const loginUser = async (req, res) => {
   try {
-    res.render('userDetails', {
-      user: await db.User.findByPk(req.params.id, {
-        include: [
-          { association: 'category' }
-        ]
-      })
-    })
+    const user = await db.User.findOne({ where: { email: req.body.email } })
+    if (!user) {
+      return res.render('registrationForm', { error: { login: { email: true } } })
+    }
+
+    if (!await compare(req.body.password, user.password)) {
+      return res.render('registrationForm', { error: { login: { password: true } } })
+    }
+
+    const token = await sign({ user: { email: user.email } }, 'our secret')
+    res.cookie('token', token)
+    res.redirect('/')
+  } catch (err) {
+    createHttpError(500)
+    console.error(err)
+  }
+}
+
+export const updateUserInfo = async (req, res) => {
+  try {
+    const payload = await verify(req.cookies.token, 'our secret')
+    const user = await db.User.findOne({ where: { email: payload.user.email } })
+
+    if (!user) {
+      throw 'User specified does not exists!'
+    }
+
+    if (req.body.firstName) {
+      user.firstName = req.body.firstName
+    }
+
+    if (req.body.lastName) {
+      user.lastName = req.body.lastName
+    }
+
+    if (req.body.email) {
+      user.email = req.body.email
+    }
+
+    if (req.body.password) {
+      user.password = hash(req.body.password, 10)
+    }
+
+    if (req.file) {
+      user.image = req.file.filename
+    }
+
+    await user.save()
+    res.redirect('/')
+  } catch (err) {
+    createHttpError(500)
+    console.error(err)
+  }
+}
+
+export const showUserProfile = async (req, res) => {
+  try {
+    const payload = await verify(req.cookies.token, 'our secret')
+    const user = await db.User.findOne({ where: { email: payload.user.email } })
+
+    if (!user) {
+      throw 'User specified does not exists!'
+    }
+
+    res.render('userProfile', { user: user })
+  } catch (err) {
+    createHttpError(500)
+    console.error(err)
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  try {
+    const payload = await verify(req.cookies.token, 'our secret')
+    const user = await db.User.findOne({ where: { email: payload.user.email } })
+
+    if (!user) {
+      throw 'User specified does not exists!'
+    }
+
+    await user.destroy()
+    res.redirect('/')
   } catch (err) {
     createHttpError(500)
     console.error(err)
