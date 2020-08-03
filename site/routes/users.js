9096668c1ec8ed3ed0c7fc70a3bd9667470f719d
gastonnicolli <49341886+gastonnicolli@users.noreply.github.com>
2020-08-03 12:41:03 -0300

    Modificacion al userRouter para hacer funcionar los post de front validation.

--- site/routes/users.js
+++ site/routes/users.js
@@ -6,4 +6,11 @@ const router = Router()
 // GET /users/register
 router.get('/register', showRegistrationForm)
 
+router.post('/register',(req, res)=>{
+  res.send('hola')
+})
+
+router.post('/login',(req, res)=>{
+  res.send('hola')
+})
 export default router
