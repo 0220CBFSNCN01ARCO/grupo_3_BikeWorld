62ae9b2ccaee74198cc30d9f4f5e7cbd2cf7dca9
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-08-05 16:34:39 -0300

    Usuarios: Implementación completada
    Se completó la implementación del controlador de usuarios, se
    implementaron las validaciones, se migró a usar el sistema de errores de
    express y se implementó el controlador en el enrutador

--- site/controllers/usersController.js
+++ site/controllers/usersController.js
@@ -1,19 +1,47 @@
 import db from '../database/models'
-import createHttpError from 'http-errors'
 import { hash, compare } from 'bcrypt'
 import { sign as _sign, verify as _verify } from 'jsonwebtoken'
 import { promisify } from 'util'
+import { validationResult } from 'express-validator'
 
 const sign = promisify(_sign)
 const verify = promisify(_verify)
 
 export const showRegistrationForm = (req, res) => res.render('registrationForm')
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
   try {
-    const user = await db.User.create({
-      firstName: req.body.firstName || '',
-      lastName: req.body.lastName || '',
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.render('registrationForm', { registrationErrors: errors.array() })
+    } else if (req.body.multerError) {
+      return res.render('registrationForm', {
+        registrationErrors: [
+          {
+            location: 'body',
+            msg: req.body.multerError.message,
+            param: 'avatar'
+          }
+        ]
+      })
+    }
+
+    let user = db.User.findOne({ where: { email: req.body.email }})
+    if (user) {
+      return res.render('registrationForm', {
+        registrationErrors: [
+          {
+            location: 'body',
+            msg: 'El email ingresado ya está registrado',
+            param: 'email'
+          }
+        ]
+      })
+    }
+
+    user = await db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
       password: await hash(req.body.password, 10),
       userAdmin: false
@@ -28,32 +56,51 @@ export const registerUser = async (req, res) => {
     res.cookie('token', token)
     res.redirect('/')
   } catch (err) {
-    createHttpError(500)
-    console.error(err)
+    next(err)
   }
 }
 
-export const loginUser = async (req, res) => {
+export const loginUser = async (req, res, next) => {
   try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.render('registrationForm', { loginErrors: errors.array() })
+    }
+
     const user = await db.User.findOne({ where: { email: req.body.email } })
     if (!user) {
-      return res.render('registrationForm', { error: { login: { email: true } } })
+      return res.render('registrationForm', {
+        loginErrors: [
+          {
+            location: 'body',
+            msg: 'El email ingresado no existe',
+            param: 'email'
+          }
+        ]
+      })
     }
 
     if (!await compare(req.body.password, user.password)) {
-      return res.render('registrationForm', { error: { login: { password: true } } })
+      return res.render('registrationForm', {
+        loginErrors: [
+          {
+            location: 'body',
+            msg: 'La contraseña ingresada es incorrecta',
+            param: 'password'
+          }
+        ]
+      })
     }
 
     const token = await sign({ user: { email: user.email } }, 'our secret')
     res.cookie('token', token)
     res.redirect('/')
   } catch (err) {
-    createHttpError(500)
-    console.error(err)
+    next(err)
   }
 }
 
-export const updateUserInfo = async (req, res) => {
+export const updateUserInfo = async (req, res, next) => {
   try {
     const payload = await verify(req.cookies.token, 'our secret')
     const user = await db.User.findOne({ where: { email: payload.user.email } })
@@ -85,12 +132,11 @@ export const updateUserInfo = async (req, res) => {
     await user.save()
     res.redirect('/')
   } catch (err) {
-    createHttpError(500)
-    console.error(err)
+    next(err)
   }
 }
 
-export const showUserProfile = async (req, res) => {
+export const showUserProfile = async (req, res, next) => {
   try {
     const payload = await verify(req.cookies.token, 'our secret')
     const user = await db.User.findOne({ where: { email: payload.user.email } })
@@ -101,12 +147,11 @@ export const showUserProfile = async (req, res) => {
 
     res.render('userProfile', { user: user })
   } catch (err) {
-    createHttpError(500)
-    console.error(err)
+    next(err)
   }
 }
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = async (req, res, next) => {
   try {
     const payload = await verify(req.cookies.token, 'our secret')
     const user = await db.User.findOne({ where: { email: payload.user.email } })
@@ -118,7 +163,6 @@ export const deleteUser = async (req, res) => {
     await user.destroy()
     res.redirect('/')
   } catch (err) {
-    createHttpError(500)
-    console.error(err)
+    next(err)
   }
 }
--- site/routes/users.js
+++ site/routes/users.js
@@ -1,15 +1,68 @@
 import { Router } from 'express'
-import { showRegistrationForm } from '../controllers/usersController'
+import { showRegistrationForm, registerUser, loginUser } from '../controllers/usersController'
+import { body } from 'express-validator'
+import multer, { diskStorage, MulterError } from 'multer'
+import { extname } from 'path'
+
+const storage = diskStorage({
+  destination: (req, file, cb) => cb(null, 'site/public/images/users/'),
+  filename: (req, file, cb) =>
+    cb(null, `${file.fieldname}-${Date.now()}${extname(file.originalname)}`)
+})
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' ||
+        file.mimetype === 'image/png' || file.mimetype === 'image/gif')
+    {
+      cb(null, true)
+    } else {
+      cb(null, false)
+      return cb(new Error('La imagen seleccionada no es válida'))
+    }
+  }
+})
 
 export const usersRouter = Router()
 
 // GET /users/register
 usersRouter.get('/register', showRegistrationForm)
 
-usersRouter.post('/register',(req, res)=>{
-  res.send('hola')
-})
+usersRouter.post('/register', [
+  body('firstName').exists({ checkFalsy: true }).withMessage('Ingrese un nombre').trim()
+    .isLength({ min: 2 }).withMessage('El nombre debe tener al menos 2 caracteres'),
+  body('lastName').exists({ checkFalsy: true }).withMessage('Ingrese un apellido').trim()
+    .isLength({ min: 2 }).withMessage('El apellido debe tener al menos 2 caracteres'),
+  body('email').exists({ checkFalsy: true }).withMessage('Ingrese un email').trim()
+    .isEmail().withMessage('El email ingresado no es válido'),
+  body('password').exists({ checkFalsy: true }).withMessage('Ingrese una contraseña').trim()
+    .isLength({ min: 8 })
+    .withMessage('La contraseña debe tener al menos 8 caracteres')
+    .matches(/[A-Z]/).withMessage('La contraseña debe tener al menos 1 letra mayúscula')
+    .matches(/[a-z]/).withMessage('La contraseña debe tener al menos 1 letra minúscula')
+    .matches(/\d/).withMessage('La contraseña debe tener al menos 1 número')
+    .matches(/\W/).withMessage('La contraseña debe tener al menos 1 caracter especial'),
+  body('passwordRepeat').trim().custom((value, { req }) => {
+    if (value !== req.body.password) {
+      return Promise.reject('Las contraseñas no coinciden')
+    }
+  }),
+], (req, res, next) => {
+  upload.single('avatar')(req, res, err => {
+    if (err instanceof MulterError) {
+      req.body.multerError = err
+      next()
+    } else if (err) {
+      next(err)
+    } else {
+      next()
+    }
+  })
+}, registerUser)
 
-usersRouter.post('/login',(req, res)=>{
-  res.send('hola')
-})
+usersRouter.post('/login', [
+  body('email').exists({ checkFalsy: true }).withMessage('Ingrese un email').trim()
+    .isEmail().withMessage('El email ingresado no es válido'),
+  body('password').exists({ checkFalsy: true }).withMessage('Ingrese una contraseña').trim()
+], loginUser)
