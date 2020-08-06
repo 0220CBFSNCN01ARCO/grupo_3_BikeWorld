f0f39f34fa9df01f71ba5dec6c2c2c176e1959ad
cdellacasa <62229574+cdellacasa@users.noreply.github.com>
2020-08-06 14:19:33 -0300

    Cart Controller:  vista dinámica archivos
    Se actualizó el controlador para que la carga de la vista sea dinámica.

--- site/controllers/cartController.js
+++ site/controllers/cartController.js
@@ -1,1 +1,14 @@
-export const showCart = (req, res) => res.render('cart')
+import { join } from 'path'
+
+
+export const showCart = (req, res) => res.render('cart', {items: [
+  {name:"Venzo Skyline 27 v", price:38791, image:"image-1592795665963.jpg",
+  description:"Venzo Skyline de 27 velocidades, frenos hidráulicos y suspensión a elastómeros."},
+  {name:"Casco", price:1576, image:"image-1592795275916.jpg",
+  description:"Casco para uso recreativo ajustable."},
+  {name:"Medidor de cadena", price:356, image:"image-1592795357323.jpg",
+  description:"Medidor de desgaste de cadena en 1 y 0.75."},
+  {name:"Piñón 12 velocidades", price:3245, image:"image-1592795451650.jpg",
+  description:"Piñón de 12 velocidades 11 / 46, cuerpo de aluminio, ligero en peso."}],
+  getProductImagePath: imageFileName => join("/images/products", imageFileName)
+})
