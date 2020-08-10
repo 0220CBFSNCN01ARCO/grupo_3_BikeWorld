a12a5b5162a3ae1b706143917057de9f50cface6
Nahuel Gomez Castro <nahual_gomca@outlook.com.ar>
2020-08-10 01:37:51 -0300

    UsersRouter: Implementada ruta de edición de usuario

--- site/routes/users.js
+++ site/routes/users.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { showRegistrationForm, registerUser, loginUser, showUserProfile } from '../controllers/usersController'
+import { showRegistrationForm, registerUser, loginUser, showUserProfile, updateUserInfo } from '../controllers/usersController'
 import { body } from 'express-validator'
 import { doNotAccessIfLoggedIn, doNotAccessIfNotLoggedIn } from '../middlewares/userRestrictionsMiddleware'
+import validator from 'validatorjs'
 
 export const usersRouter = Router()
 
@@ -41,3 +42,39 @@ usersRouter.post('/login', doNotAccessIfLoggedIn, [
     .isEmail().withMessage('El email ingresado no es válido'),
   body('password').exists({ checkFalsy: true }).withMessage('Ingrese una contraseña').trim()
 ], loginUser)
+
+// PUT /users/:id
+usersRouter.put('/:id', doNotAccessIfNotLoggedIn, [
+  body('firstName').exists({ checkFalsy: true }).withMessage('Ingrese un nombre').trim()
+    .isLength({ min: 2 }).withMessage('El nombre debe tener al menos 2 caracteres'),
+  body('lastName').exists({ checkFalsy: true }).withMessage('Ingrese un apellido').trim()
+    .isLength({ min: 2 }).withMessage('El apellido debe tener al menos 2 caracteres'),
+  body('email').exists({ checkFalsy: true }).withMessage('Ingrese un email').trim()
+    .isEmail().withMessage('El email ingresado no es válido'),
+  body('password').custom(value => {
+    if (value !== undefined && value !== '' && value !== 0 && value !== false) {
+      if (!validator.isLength(value, { min: 8 })) {
+        throw new Error('La contraseña debe tener al menos 8 caracteres')
+      } else if (!validator.matches(value, /[A-Z]/)) {
+        throw new Error('La contraseña debe tener al menos 1 letra mayúscula')
+      } else if (!validator.matches(value, /[a-z]/)) {
+        throw new Error('La contraseña debe tener al menos 1 letra minúscula')
+      } else if (!validator.matches(value, /\d/)) {
+        throw new Error('La contraseña debe tener al menos 1 número')
+      }
+
+      return true
+    }
+  }),
+  body('passwordRepeat').custom((value, { req }) => {
+    if (req.body.password !== undefined && req.body.password !== '' && req.body.password !== 0 && req.body.password !== false) {
+      if (value === undefined || value === '' || value === 0 || value === false) {
+        throw new Error('Repita la contraseña')
+      } else if (value !== req.body.password) {
+        throw new Error('Las contraseñas no coinciden')
+      }
+
+      return true
+    }
+  })
+], updateUserInfo)
